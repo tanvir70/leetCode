aa195e713cbb305a3145f5f317872cf950654824
Tanvir Anjum Rifat <80834028+tanvir70@users.noreply.github.com>
2024-02-16 03:12:50 +0600

    Time: 1 ms (98.12%) | Memory: 45.6 MB (42.88%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int leftSum = 0;
+        int totalSum = 0;
+
+        for (int num : nums){
+            totalSum += num;
+        }
+
+        for ( int i = 0; i < nums.length; i++){
+            if ( leftSum == (totalSum - leftSum - nums[i])){
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return - 1;
+    }
+}
