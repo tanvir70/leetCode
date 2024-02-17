17f4445378ce29aabf64d945211c4fb6dce69f10
Tanvir Anjum Rifat <80834028+tanvir70@users.noreply.github.com>
2024-02-17 22:21:38 +0600

    Time: 14 ms (36.59%) | Memory: 45.2 MB (85.69%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -1,29 +1,26 @@
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
        Set<Integer> set1 = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
+       Set<Integer> set2 = new HashSet<>();
 
-        for (int num : nums1) {
-            set1.add(num);
-        }
+       for ( int num : nums1){
+           set1.add(num);
+       }
+       for ( int num : nums2){
+           set2.add(num);
+       }
 
-        for (int num : nums2) {
-            set2.add(num);
-        }
+       Set<Integer> difference1 = new HashSet<>(set1);
+       difference1.removeAll(set2);
 
-        // Find elements in nums1 not present in nums2
-        Set<Integer> difference1 = new HashSet<>(set1);
-        difference1.removeAll(set2);
+       Set<Integer> difference2 = new HashSet<>(set2);
+       difference2.removeAll(set1);
 
-        // Find elements in nums2 not present in nums1
-        Set<Integer> difference2 = new HashSet<>(set2);
-        difference2.removeAll(set1);
+        List<List<Integer>> answer = new ArrayList<>();
+        answer.add(new ArrayList<>(difference1));
+        answer.add(new ArrayList<>(difference2));
 
-        // Create lists to hold the results
-        List<List<Integer>> result = new ArrayList<>();
-        result.add(new ArrayList<>(difference1));  // Convert set to list
-        result.add(new ArrayList<>(difference2));
+        return answer;
 
-        return result;
     }
 }
