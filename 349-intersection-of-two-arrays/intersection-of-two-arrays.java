4601c84ed6115c412e5c3b9a8d5df7e7dde227b3
Tanvir Anjum Rifat <80834028+tanvir70@users.noreply.github.com>
2024-06-30 11:55:59 +0600

    Time: 3 ms (54.68%) | Memory: 43.3 MB (26.72%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] intersection(int[] arrN, int[] arrM) {
+     
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> commonElements = new HashSet<>();
+        
+        for (int i = 0; i < arrN.length; i++) {
+            set.add(arrN[i]);
+        }
+
+         for (int i = 0; i < arrM.length; i++) {
+            if (set.contains(arrM[i])){
+                commonElements.add(arrM[i]);
+            }
+        }
+
+        int[] result = new int[commonElements.size()];
+        int index = 0;
+        
+        for (int element : commonElements) {
+            result[index++] = element;
+        }
+   
+        Arrays.sort(result);
+        
+        return result;
+    }
+}
