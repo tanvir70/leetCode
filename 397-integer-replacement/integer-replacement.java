31c6c1c1143fbd1156043eedfbc661c68c669612
Tanvir Anjum Rifat <80834028+tanvir70@users.noreply.github.com>
2024-07-11 05:29:38 +0600

    Time: 1 ms (67.85%) | Memory: 40.4 MB (28.35%) - LeetSync

--- 397-integer-replacement/integer-replacement.java
+++ 397-integer-replacement/integer-replacement.java
@@ -1,37 +1,33 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-    private Map<Integer, Integer> memo = new HashMap<>();
-    
+    private HashMap<Integer,Integer> memo = new HashMap<>();
     public int integerReplacement(int n) {
-        // Handle the edge case where n is Integer.MAX_VALUE
-        if (n == Integer.MAX_VALUE) {
-            return 32;  // Since Integer.MAX_VALUE is 2^31 - 1, it takes 32 steps to reduce it to 1
+        if (n == Integer.MAX_VALUE){
+            return 32;
         }
-        
-        return integerReplacementHelper(n);
+
+        return helper(n);
     }
-    
-    private int integerReplacementHelper(int n) {
-        if (n == 1) {
+
+    private int helper(int n){
+        if (n == 1){
             return 0;
         }
-        
-        if (memo.containsKey(n)) {
+
+        if (memo.containsKey(n)){
             return memo.get(n);
         }
         
-        int count;
-        if (n % 2 == 0) {
-            count = 1 + integerReplacementHelper(n / 2);
-        } else {
-            int increment = 1 + integerReplacementHelper(n + 1);
-            int decrement = 1 + integerReplacementHelper(n - 1);
-            count = Math.min(increment, decrement);
+        int count = 0;
+      
+        if ( n % 2 == 0){
+           count = 1 + helper(n / 2);
+        }else {
+            int dif1 = 1 + integerReplacement(n + 1);
+            int dif2 = 1 + integerReplacement(n - 1) ;
+            count = Math.min(dif1,dif2);
         }
-        
-        memo.put(n, count);
+
+        memo.put(n,count);
         return count;
     }
 }
