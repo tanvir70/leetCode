1f7d1bcb690eeb82d09f4168ab0f60bf166b1e29
Tanvir Anjum Rifat <80834028+tanvir70@users.noreply.github.com>
2024-07-10 12:03:13 +0600

    Time: 25 ms (95.81%) | Memory: 51.4 MB (62.43%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,26 +1,33 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        Set<List<Integer>> uniqueTriplets = new HashSet<>();
-        List<List<Integer>> result = new ArrayList<>();
-        for (int i = 0; i < nums.length; ++i) {
-            int left = i + 1;
-            int right = nums.length - 1;
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum == 0) {
-                    uniqueTriplets.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    ++left;
-                    --right;
-                } else if (sum > 0) {
-                    --right;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int total = nums[i] + nums[j] + nums[k];
+
+                if (total > 0) {
+                    k--;
+                } else if (total < 0) {
+                    j++;
                 } else {
-                    ++left;
-                }}}
-        result.addAll(uniqueTriplets);
-        return result;}}
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+
+                    while (nums[j] == nums[j-1] && j < k) {
+                        j++;
+                    }
+                }
+            }
+        }
+        return res;        
+    }
+}
