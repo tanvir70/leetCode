91859f4f0b45edc02bdec9204c74fc6314222cc3
Tanvir Anjum Rifat <80834028+tanvir70@users.noreply.github.com>
2024-07-10 23:53:19 +0600

    Time: 0 ms (100.00%) | Memory: 40.3 MB (26.57%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,9 +1,7 @@
 class Solution {
     public int fib(int n) {
-        if ( n <= 1){
-            return n;
-        }
-
-        return fib(n - 1) + fib(n - 2);
+        double sqrt5 = Math.sqrt(5);
+        double fibN = Math.pow((1 + sqrt5) / 2, n) - Math.pow((1 - sqrt5) / 2, n);
+        return (int) Math.round(fibN / sqrt5);
     }
 }
