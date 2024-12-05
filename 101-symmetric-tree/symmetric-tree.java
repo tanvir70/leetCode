a67d01997129d84a11d09d5463a2f425ced27b3b
Tanvir Anjum Rifat <80834028+tanvir70@users.noreply.github.com>
2024-12-05 23:29:08 +0600

    Time: 0 ms (100.00%) | Memory: 41.6 MB (73.67%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -1,15 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null) return true; // An empty tree is symmetric
-        return isMirror(root.left, root.right);
+        return isMirror(root.left,root.right);
     }
 
-    private boolean isMirror(TreeNode p, TreeNode q) {
-        if (p == null && q == null) return true; // Both nodes are null, symmetric
-        if (p == null || q == null) return false; // One is null, not symmetric
-        if (p.val != q.val) return false; // Values do not match, not symmetric
+    private boolean isMirror(TreeNode p, TreeNode q){
+        if (p == null &&  q == null) return true;
+        if (p == null ||  q == null) return false;
+        if (p.val != q.val) return false;
 
-        // Compare left subtree of `p` with right subtree of `q` and vice versa
-        return isMirror(p.left, q.right) && isMirror(p.right, q.left);
+        return isMirror(p.left,q.right) && isMirror(p.right,q.left);
     }
 }
